dd40fcc99fca553dfefc65fcd97d74910cea51ed
Calculator <chyfahim0998@gmail.com>
2026-07-05 12:09:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let display = document.getElementById('display');
+let expression = '';
+let justCalculated = false;
+
+function updateDisplay() {
+  display.value = expression.replace(/\*/g, '×').replace(/\//g, '÷');
+}
+
+function appendValue(value) {
+  if (justCalculated) {
+    expression = '';
+    justCalculated = false;
+  }
+  expression += value;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  expression = '';
+  justCalculated = false;
+  updateDisplay();
+}
+
+function deleteLast() {
+  expression = expression.slice(0, -1);
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    expression = eval(expression).toString();
+    justCalculated = true;
+    updateDisplay();
+  } catch {
+    display.value = 'Error';
+    expression = '';
+    justCalculated = true;
+  }
+}
+
+function calculatePercent() {
+  try {
+    expression = (eval(expression) / 100).toString();
+    justCalculated = true;
+    updateDisplay();
+  } catch {
+    display.value = 'Error';
+    expression = '';
+    justCalculated = true;
+  }
+}
